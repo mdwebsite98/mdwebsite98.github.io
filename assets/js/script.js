0933b3b6a7ac521247e1625d3956f8aa6dd1cb3a
mdwebsite98 <eshitxxx2@gmail.com>
2026-03-03 21:58:12 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,32 @@ menuBtn.addEventListener('click', () => {
   console.log('Mở Menu hệ thống...');
   // Bạn có thể thêm code mở Side Menu ở đây sau nhé
 });
+document.addEventListener('DOMContentLoaded', function() {
+  // Cấu hình cho slide bên trái
+  const swiperLeft = new Swiper('.swiper-left', {
+    loop: true,
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-left .swiper-pagination',
+      clickable: true,
+    },
+    grabCursor: true, // Hiện bàn tay khi di chuột vào để biết là kéo được
+  });
+
+  // Cấu hình cho slide bên phải
+  const swiperRight = new Swiper('.swiper-right', {
+    loop: true,
+    autoplay: {
+      delay: 4500, // Cho 2 bên lệch nhau chút nhìn cho nghệ
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-right .swiper-pagination',
+      clickable: true,
+    },
+    grabCursor: true,
+  });
+});
